8e62f241c947d991b6e175980f70db3f7db85101
Aadhavan P <84095187+Aadhavancnp@users.noreply.github.com>
2024-07-02 13:54:44 +0000

    Add SEO Meta Tags & Centered Spinner

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,21 @@
-import React, { useState } from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Home from './Pages/Home/index.jsx'
-import DashboardLayout from './Layouts/DashboardLayout'
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/index.jsx";
+import DashboardLayout from "./Layouts/DashboardLayout";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
       <Routes>
-        <Route path="/" element={
-          <DashboardLayout>
-            <Home/>
-          </DashboardLayout>
-        } />
+        <Route
+          path="/"
+          element={
+            <DashboardLayout>
+              <Home />
+            </DashboardLayout>
+          }
+        />
       </Routes>
-        
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
